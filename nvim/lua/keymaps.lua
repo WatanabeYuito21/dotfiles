0326e0870857161c5cc13d5093395faa2834104f
WatanabeYuito21 <Watanabe.Yuito21@gmail.com>
2025-08-04 13:46:54 +0900

    fix: 以下のようにcopilotの有効無効を切り替えられるようにした。 <leader>cpe : copilot enable <leader>cpd : copilot disable <leader>cps : copilot status

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -7,3 +7,8 @@ map('i', 'jj', '<ESC>', opts_noremap)
 map('n', '<ESC><ESC>', ':nohl<CR>', opts)
 map('n', 'Y', 'y$', opts_noremap)
 map('t', 'jj', [[<C-\><C-n]], opts_noremap)
+
+-- Copilot関連のキーマップ
+vim.keymap.set('n', '<leader>cpe', ':Copilot enable<CR>', { silent = true, desc = 'Enable Copilot' })
+vim.keymap.set('n', '<leader>cpd', ':Copilot disable<CR>', { silent = true, desc = 'Disable Copilot' })
+vim.keymap.set('n', '<leader>cps', ':Copilot status<CR>', { silent = true, desc = 'Copilot Status' })
