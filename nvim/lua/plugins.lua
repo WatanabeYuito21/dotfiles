1157eb5377ad5faad32e02b869fee71de67e5dcc
WatanabeYuito21 <Watanabe.Yuito21@gmail.com>
2025-10-20 08:34:44 +0900

    add: memo.vim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -66,6 +66,28 @@ M.editor_plugins = {
         dependencies = { 'nvim-treesitter/nvim-treesitter' },
         opts = {}
     },
+    {
+        'glidenote/memolist.vim',
+        cmd = {
+            'MemoNew',
+            'MemoList',
+            'MemoGrep',
+        },
+        keys = {
+            {'<Leader>mn', '<cmd>MemoNew<cr>', desc = 'New Memo'},
+            {'<Leader>ml', '<cmd>MemoList<cr>', desc = 'List Memos'},
+            {'<Leader>mg', '<cmd>MemoGrep<cr>', desc = 'Grep Memos'},
+        },
+        config = function()
+            vim.g.memolist_path = vim.fn.expand('~/memolist')
+            vim.g.memolist_memo_suffix = 'txt'
+            vim.g.memolist_memo_date = '%Y%m%d_%H:%M'
+            vim.g.memolist_filename_prefix_none = 0
+            vim.g.memolist_template_dir_path = vim.fn.expand('~/.config/nvim/templates')
+            vim.g.memolist_memo_list_height = 15
+            vim.g.memolist_ex_cmd = 'CtrlP'
+        end,
+    },
 }
 
 -- ファイル管理プラグイン
