1c79f8820f90d98752c6c7597d386d3cb9e7009d
WatanabeYuito21 <Watanabe.Yuito21@gmail.com>
2025-10-20 12:28:01 +0900

    fix: neotreeでlist表示するよう変更

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -78,12 +78,21 @@ M.editor_plugins = {
             {'<Leader>ml', '<cmd>MemoList<cr>', desc = 'List Memos'},
             {'<Leader>mg', '<cmd>MemoGrep<cr>', desc = 'Grep Memos'},
         },
-        config = function()
+        init = function()
             local memolist_path
-            if vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1 then
+
+            local is_wsl = vim.fn.filereadable('/proc/version') == 1 and vim.fn.readfile('/proc/version')[1]:match('microsoft') ~=nil
+
+            if is_wsl or vim.fn.has('unix') == 1 then
+                memolist_path = vim.fn.expand('~/Documents/memolist')
+            elseif vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1 then
                 memolist_path = vim.fn.expand('~/Documents/memolist')
             else
                 memolist_path = vim.fn.expand('~/Documents/memolist')
+            end
+
+            if vim.fn.isdirectory(memolist_path) == 0 then
+                vim.fn.mkdir(memolist_path, 'p')
             end
 
             vim.g.memolist_path = memolist_path
@@ -92,7 +101,7 @@ M.editor_plugins = {
             vim.g.memolist_filename_prefix_none = 0
             vim.g.memolist_template_dir_path = vim.fn.expand('~/.config/nvim/templates')
             vim.g.memolist_memo_list_height = 15
-            vim.g.memolist_ex_cmd = 'CtrlP'
+            vim.g.memolist_ex_cmd = 'Neotree'
             vim.g.memolist_filename_date = '%Y%m%d_'
         end,
     },
