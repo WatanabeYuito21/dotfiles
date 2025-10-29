d9bbaec4319ec19da69e2d212ce833c224b13ef7
WatanabeYuito21 <Watanabe.Yuito21@gmail.com>
2025-10-30 05:11:14 +0900

    fix: typo

--- nvim/lua/plugins/file_management.lua
+++ nvim/lua/plugins/file_management.lua
@@ -2,21 +2,26 @@
 -- -- ファイル管理関連のプラグイン設定
 
 return {
-    'nvim-neo-tree/neo-tree.nvim',
-    branch = 'v3.x',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        'nvim-tree/nvim-web-devicons',
-        'MunifTanjim/nui.nvim',
-    },
-    opts = {
-        filesystem = {
-            follow_current_file = { enabled = true },
-            use_libuv_file_watcher = true,
-            filtered_items = {
-                hide_dotfiles = false,
-                visible = true,
-                hide_gitignored = false,
+    {
+        'nvim-neo-tree/neo-tree.nvim',
+        branch = 'v3.x',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'nvim-tree/nvim-web-devicons',
+            'MunifTanjim/nui.nvim',
+        },
+        opts = {
+            filesystem = {
+                follow_current_file = { enabled = true },
+                use_libuv_file_watcher = true,
+                filtered_items = {
+                    visible = true,
+                    hide_dotfiles = false,
+                    hide_gitignored = false,
+                    hide_ignored = false,
+                    hide_hidden = false,
+                    always_show_by_pattern = {"*"},
+                },
             },
         },
     },
