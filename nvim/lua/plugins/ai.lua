8871053ad9e4d1d6e99b6ce2d07a2a5f907f7fec
WatanabeYuito21 <Watanabe.Yuito21@gmail.com>
2025-11-04 16:02:14 +0900

    fix: avante.nvim をVeryLazyで読み込むようにした。

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -5,7 +5,7 @@ return {
     {
         "yetone/avante.nvim",
         event = "VeryLazy",
-        lazy = false,
+        -- lazy = false,
         version = false,
         build = "make",
         opts = {
