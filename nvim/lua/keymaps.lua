1a632a250de915343eb6d899b00c61de2a90d67c
WatanabeYuito21 <Watanabe.Yuito21@gmail.com>
2026-04-02 08:52:45 +0900

    fix: copilot to claude

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -8,15 +8,15 @@ map('n', '<ESC><ESC>', ':nohl<CR>', opts)
 map('n', 'Y', 'y$', opts_noremap)
 map('t', 'jj', [[<C-\><C-n]], opts_noremap)
 
--- Copilot関連のキーマップ
-vim.keymap.set('n', '<leader>cpe', ':Copilot enable<CR>', { silent = true, desc = 'Enable Copilot' })
-vim.keymap.set('n', '<leader>cpd', ':Copilot disable<CR>', { silent = true, desc = 'Disable Copilot' })
-vim.keymap.set('n', '<leader>cps', ':Copilot status<CR>', { silent = true, desc = 'Copilot Status' })
-
--- Avante.nvim関連のキーマップ
-vim.keymap.set('n', '<leader>aa', ':AvanteAsk<CR>', { silent = true, desc = 'Avante Ask' })
-vim.keymap.set('v', '<leader>aa', ':AvanteAsk<CR>', { silent = true, desc = 'Avante Ask (Visual)' })
-vim.keymap.set('n', '<leader>ar', ':AvanteRefresh<CR>', { silent = true, desc = 'Avante Refresh' })
-vim.keymap.set('n', '<leader>ae', ':AvanteEdit<CR>', { silent = true, desc = 'Avante Edit' })
-vim.keymap.set('n', '<leader>at', ':AvanteToggle<CR>', { silent = true, desc = 'Avante Toggle' })
-vim.keymap.set('n', '<leader>af', ':AvanteFocus<CR>', { silent = true, desc = 'Avante Focus' })
+-- -- Copilot関連のキーマップ
+-- vim.keymap.set('n', '<leader>cpe', ':Copilot enable<CR>', { silent = true, desc = 'Enable Copilot' })
+-- vim.keymap.set('n', '<leader>cpd', ':Copilot disable<CR>', { silent = true, desc = 'Disable Copilot' })
+-- vim.keymap.set('n', '<leader>cps', ':Copilot status<CR>', { silent = true, desc = 'Copilot Status' })
+--
+-- -- Avante.nvim関連のキーマップ
+-- vim.keymap.set('n', '<leader>Ca', ':AvanteAsk<CR>', { silent = true, desc = 'Avante Ask' })
+-- vim.keymap.set('v', '<leader>Ca', ':AvanteAsk<CR>', { silent = true, desc = 'Avante Ask (Visual)' })
+-- vim.keymap.set('n', '<leader>Cr', ':AvanteRefresh<CR>', { silent = true, desc = 'Avante Refresh' })
+-- vim.keymap.set('n', '<leader>Ce', ':AvanteEdit<CR>', { silent = true, desc = 'Avante Edit' })
+-- vim.keymap.set('n', '<leader>Ct', ':AvanteToggle<CR>', { silent = true, desc = 'Avante Toggle' })
+-- vim.keymap.set('n', '<leader>Cf', ':AvanteFocus<CR>', { silent = true, desc = 'Avante Focus' })
